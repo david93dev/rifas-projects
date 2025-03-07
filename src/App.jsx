6483cb6f96b22e9960df8821956720d88a240be1
david93dev <david.flor@maisunifacisa.com.br>
2025-03-07 09:05:36 -0300

    tela sobre nós

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Ganhadores } from "./pages/ganhadores/Ganhadores";
+import { QuemSomos } from "./pages/sobre-nos/QuemSomos";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="Ganhadores" element={<Ganhadores />} />
+      <Route path="Quem-somos" element={<QuemSomos />} />
     </Routes>
   );
 }
---
+++ src/pages/sobre-nos/QuemSomos.jsx
@@ -0,0 +1,14 @@
+import { Header } from '../../components/header/Header'
+import { Footer } from '../../components/footer/Footer'
+import { AboutUs } from '../../components/about-us/AboutUs'
+
+export function QuemSomos() {
+  return (
+   <>
+   <Header />
+   <AboutUs />
+   <Footer />
+   </>
+  )
+}
+
